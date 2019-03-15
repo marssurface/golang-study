242dcc3b5edc96f00a10baa06bdd38e27acf4358
jiangpeng <646274869@qq.com>
2019-03-15 23:46:30 +0800

    练习 ch1 1.4

--- ch1/dup4/main.go
+++ ch1/dup4/main.go
@@ -1,1 +1,39 @@
-// 练习 1.4： 修改dup2，出现重复的行时打印文件名称。package mainimport (	"bufio"	"fmt"	"os")func main()  {	files := os.Args[1:]	// 从文件读入	for _, arg := range files {		// os.Open函数返回两个值。第一个值是被打开的文件(*os.File）		f, err := os.Open(arg)		if err != nil {			fmt.Fprintf(os.Stderr, "dup 2: %v\n", err)			continue		}		countLines(f, arg)		f.Close()	}}func countLines(f *os.File, filename string)  {	counts := make(map[string]int)	input := bufio.NewScanner(f)	for input.Scan() {		counts[input.Text()]++	}	for _, n := range counts {		if n > 1 {			fmt.Printf("filename %s have repeat line\n", filename)			break		}	}}
+// 练习 1.4： 修改dup2，出现重复的行时打印文件名称。
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main()  {
+	files := os.Args[1:]
+
+	// 从文件读入
+	for _, arg := range files {
+		// os.Open函数返回两个值。第一个值是被打开的文件(*os.File）
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup 2: %v\n", err)
+			continue
+		}
+		countLines(f, arg)
+		f.Close()
+	}
+}
+
+func countLines(f *os.File, filename string)  {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for _, n := range counts {
+		if n > 1 {
+			fmt.Printf("filename %s have repeat line\n", filename)
+			break
+		}
+	}
+}
